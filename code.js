810fe38675ae3453f0289f0155c8c1e2fb532656
Edxael <zerphyr.r@gmail.com>
2017-03-29 00:11:30 -0600

    general update + JS Code start

--- code.js
+++ code.js
@@ -1,1 +1,14 @@
-console.log("mass ef");
+console.log("Octo 2");
+
+
+function dive(lic, mCat){
+    var oxyTank = 5;
+    var totalCatch = mCat;
+    return function (){
+        oxyTank -= 1;
+        totalCatch += lic;
+        return [lic, mCat, oxyTank, totalCatch];
+    }
+}
+
+var mat = new dive(3, 2);
